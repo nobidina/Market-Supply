fc4cde171a284e6567055cf7d2c3a4590de70ac5
Nadezhda Obidina <etemity@yandex.ru>
2017-06-17 17:02:31 +0300

    many common fixes

--- js/menu.js
+++ js/menu.js
@@ -1,30 +1,30 @@
 (function() {
   var menu = {
-    elem: document.querySelector('.main-menu__list'),
-    button: document.querySelector('.main-menu__toggle'),
+    elem: document.querySelector('.dropdown-menu__list'),
+    button: document.querySelector('.dropdown-menu__toggle'),
     isOpened: false,
 
     open: function() {
       var btnClasses = this.button.classList;
-      btnClasses.add('main-menu__toggle_close');
+      btnClasses.add('dropdown-menu__toggle_close');
 
-      this.elem.classList.remove('main-menu__list_hidden');
+      this.elem.classList.remove('dropdown-menu__list_hidden');
       this.isOpened = true;
     },
 
     close: function() {
       var btnClasses = this.button.classList;
-      btnClasses.remove('main-menu__toggle_close');
+      btnClasses.remove('dropdown-menu__toggle_close');
 
-      this.elem.classList.add('main-menu__list_hidden');
+      this.elem.classList.add('dropdown-menu__list_hidden');
       this.isOpened = false;
     },
 
     init: function() {
       var self = this;
 
-      this.elem.classList.add('main-menu__list_hidden');
-      this.button.classList.remove('main-menu__toggle_no-js');
+      this.elem.classList.add('dropdown-menu__list_hidden');
+      this.button.classList.remove('dropdown-menu__toggle_no-js');
 
       this.button.onclick = function() {
         if (!self.isOpened)
@@ -35,7 +35,7 @@
     },
 
     off: function() {
-      this.button.classList.remove('main-menu__toggle_no-js');
+      this.button.classList.remove('dropdown-menu__toggle_no-js');
 
       this.button.onclick = null;
     }
